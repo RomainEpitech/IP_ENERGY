442b8aff3b03d900f58a0c265ea6e4e3616efae3
Saskoue <romain.farinacci@hotmail.com>
2024-06-02 09:18:56 +0200

    user: update method

--- back/app/Http/Controllers/UserController.php
+++ back/app/Http/Controllers/UserController.php
@@ -2,11 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
 
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+        Log::info('Request received for user update:', $request->all());
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'nullable|string|max:255',
+            'firstname' => 'nullable|string|max:255',
+            'email' => 'nullable|string|email|max:255|unique:users,email,' . $user->id,
+            'password' => 'nullable|string|min:8|confirmed',
+            'password_confirmation' => 'nullable|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if ($request->filled('name')) {
+            $user->name = $request->name;
+        }
+        if ($request->filled('firstname')) {
+            $user->firstname = $request->firstname;
+        }
+        if ($request->filled('email')) {
+            $user->email = $request->email;
+        }
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->password);
+            Log::info('Password updated for user:', ['user_id' => $user->id]);
+        }
+
+        Log::info('User after update:', $user->toArray());
+        $user->save();
+
+        return response()->json(['user' => $user, 'message' => 'User updated successfully'], 200);
+    }
 
 
     public function destroy(){
--- back/routes/api.php
+++ back/routes/api.php
@@ -11,4 +11,5 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::delete('/delete', [UserController::class, 'destroy']);
+    Route::put('/update', [UserController::class, 'update']);
 });
